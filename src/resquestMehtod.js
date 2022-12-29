278dadfcbc2f150aead625dfe5808b7385eb37bb
Vishal Sharma <90819741+Anshusharma17@users.noreply.github.com>
2022-12-29 18:40:06 +0530

    Update resquestMehtod.js

--- src/resquestMehtod.js
+++ src/resquestMehtod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://e-commerce-server-f0f5.onrender.com/api/"; 
+const BASE_URL = "https://e-commerce-server-v980.onrender.com/api/"; 
 const access = JSON?.parse(localStorage?.getItem("persist:root"))?.user;
 let TOKEN = "";
 if (access !== undefined) {
